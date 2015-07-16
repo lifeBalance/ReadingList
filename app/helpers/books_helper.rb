4d4381c4415592696fb6f7a3066426020ec814db
Javier Rodriguez <javierorodriguez@gmail.com>
2015-07-17 02:18:38 +0300

    Finished

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,6 @@
 module BooksHelper
   def image_from_amazon(amazon_id)
-    image_tag "http://images.amazon.com/images/P/#{amazon_id}.01.ZTZZZZZZ.jpg"
+    # image_tag "http://images.amazon.com/images/P/#{amazon_id}.01.ZTZZZZZZ.jpg"
+    image_tag "http://ecx.images-amazon.com/images/I/#{amazon_id}._AA160_.jpg"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,38 @@
-Genre.destroy_all
-# 
-# genre_attrs = [{
-#   name: 'Science Fiction'
-# },
-# {
-#   name: 'Fiction'
-# },
-# {
-#   name: 'Non-Fiction'
-# }]
-
-# Genre.create!(genre_attrs)
-porn = Genre.create!(name: 'Porn')
-softcore = Genre.create!(name: 'Softcore')
-bukkake = Genre.create!(name: 'Bukkake')
-
 Book.destroy_all
+Genre.destroy_all
+
+fiction = Genre.create!(name: 'Fiction')
+apple = Genre.create!(name: 'Apple')
+non_fiction = Genre.create!(name: 'Non-Fiction')
 
 book_attrs = [{
   title: 'Mindstorms',
   author: 'Seymour A. Papert',
   description: 'Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.',
-  amazon_id: '0465046746',
+  amazon_id: '516HdDm2C0L',
   rating: 5,
   finished_on: 10.days.ago,
-  # genre: Genre.last
-  genres: [porn, bukkake]
+  genres: [fiction]
 },
 {
   title: 'Hyperion',
   author: 'Dan Simmons',
   description: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
-  amazon_id: '0553283685',
+  amazon_id: '51AGsrzE0zL',
   rating: 4,
   finished_on: 1.days.ago,
-  # genre: Genre.first
-  genres: [softcore]
+  genres: [non_fiction, apple]
 },
 {
   title: "Jony Ive: The Genius Behind Apple's Greatest Products",
   author: 'Leander Kahney',
   description: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
-  amazon_id: '159184617X',
+  amazon_id: '41b10lM5DLL',
   rating: 2,
   finished_on: nil,
-  # genre: Genre.last
-  genres: [porn]
+  genres: [non_fiction]
 }]
 
 Book.create!(book_attrs);
+
+p "Created #{Book.count} books"
