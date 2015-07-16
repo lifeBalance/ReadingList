04a6925ff510550e531e23f190c4d1214a98d0a3
Javier Rodriguez <javierorodriguez@gmail.com>
2015-07-16 15:45:22 +0300

    add scopes 'finished' and 'recent' to 'Book' model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,19 @@
 class Book < ActiveRecord::Base
+  # Scopes allow us to make queries which can be referenced as 
+  # method calls on the model. They work like class methods:
+  #     scope :finished, lambda { where('finished_on IS NOT NULL') }
+  # We can make it not so SQL:"
+  scope :finished, lambda { where.not(:finished_on => nil) }
+
+  # Another syntax for lambdas:
+  scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  
+  # Let's rewrite ':recent' as a class method:
+  # 
+  # def self.recent
+  #   where('finished_on > ?', 2.days.ago)
+  # end
+
   def finished?
     finished_on.present? 
   end
