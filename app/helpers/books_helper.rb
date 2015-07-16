37d3848481c9ecff11f9b85307cc2b2a0dcf2fcd
Javier Rodriguez <javierorodriguez@gmail.com>
2015-07-17 02:29:18 +0300

    Finished, really

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,5 @@
 module BooksHelper
   def image_from_amazon(amazon_id)
-    # image_tag "http://images.amazon.com/images/P/#{amazon_id}.01.ZTZZZZZZ.jpg"
-    image_tag "http://ecx.images-amazon.com/images/I/#{amazon_id}._AA160_.jpg"
+    image_tag "http://images.amazon.com/images/P/#{amazon_id}.01.ZTZZZZZZ.jpg"
   end
 end
